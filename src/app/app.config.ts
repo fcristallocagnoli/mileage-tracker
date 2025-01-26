b073130417e8628ebd13cecaa6bdcf46eb283a4f
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-01-26 05:52:28 +0100

    feat: add firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp } from 'firebase/app';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
+initializeApp(environment.firebaseConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      AngularFirestoreModule
+    )
+  ]
 };
