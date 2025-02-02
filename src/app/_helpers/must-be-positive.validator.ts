f25b792a6a174737fe684947dee2617e5c3ac34c
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-02-02 05:24:54 +0100

    feat: add custom validators

---
+++ src/app/_helpers/must-be-positive.validator.ts
@@ -0,0 +1,29 @@
+import { AbstractControl } from '@angular/forms';
+
+// custom validator to check that tripEndKm is greater than tripStartKm
+export function MustBePositive() {
+  return (group: AbstractControl) => {
+    const tripStartKm = group.get('tripStartKm');
+    const tripEndKm = group.get('tripEndKm');
+
+    if (!tripStartKm || !tripEndKm) {
+      return null;
+    }
+
+    // return if another validator has already found an error on the tripEndKm
+    if (tripEndKm.errors && !tripEndKm.errors['mustBePositive']) {
+      return null;
+    }
+
+    const startKmValue = Number(tripStartKm.value);
+    const endKmValue = Number(tripEndKm.value);
+
+    // set error on tripEndKm if validation fails
+    if (startKmValue > endKmValue) {
+      tripEndKm.setErrors({ mustBePositive: true });
+    } else {
+      tripEndKm.setErrors(null);
+    }
+    return null;
+  }
+}
