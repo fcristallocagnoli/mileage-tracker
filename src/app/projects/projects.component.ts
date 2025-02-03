1cde99c96c7df74560bc5d4b1b2fd57679df258a
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-02-03 04:55:59 +0100

    feat: add joining and creating projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,32 +1,98 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertService } from '@app/services/alert.service';
+
+import { ProjectCreation, ProjectDTO, ProjectJoin } from '@app/interfaces/project.interface';
+import { AuthService } from '@app/services/auth.service';
+import { BackendService } from '@app/services/backend.service';
+import { format, monthEnd, monthStart } from "@formkit/tempo";
+import { ViewProjectComponent } from "./view-project/view-project.component";
 
 @Component({
   selector: 'app-projects',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, ViewProjectComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export default class ProjectsComponent {
-  userProjects: any[] = [];
-  // userProjects: any[] = [
-  //   {
-  //     projectID: "888ddb41-efd2-5d48-903d-c114afff17a7",
-  //     password: "7a0378360df7d29729b32da4f8ba21f71a60548c7fe7e701fecfa537cacc3170",
-  //     projectName: "Project 1",
-  //     projectDescription: "This is project 1",
-  //     projectParticipants: [],
-  //     trips: []
-  //   },
-  //   {
-  //     projectID: "1bc69f86-45ea-5bad-b8a1-9841b9d08bcf",
-  //     password: "94ce1c38b3acc9946657f8a1ee3b6deb3f5a0be04cf156ec84e72dc85a8c64ba",
-  //     projectName: "Project 2",
-  //     projectDescription: "This is project 2",
-  //     projectParticipants: [],
-  //     trips: []
-  //   }
-  // ];
+export default class ProjectsComponent implements OnInit {
+  userProject?: ProjectDTO | null;
+
+  currentUser = this.authService.accountValue;
+  currentUserId = this.authService.accountValue?.uid!;
+
+  submitting = false;
+
+  joinForm = new FormGroup({
+    projectId: new FormControl('', Validators.required),
+    projectPassword: new FormControl('', Validators.required)
+  })
+
+  createForm = new FormGroup({
+    projectName: new FormControl('', Validators.required),
+    projectPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    projectStartDate: new FormControl(format(monthStart(new Date()), "YYYY-MM-DD"), Validators.required),
+    projectEndDate: new FormControl(format(monthEnd(new Date()), "YYYY-MM-DD"), Validators.required),
+    projectDescription: new FormControl(''),
+  })
+
+  constructor(
+    private authService: AuthService,
+    private backendService: BackendService,
+    private alertService: AlertService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.backendService.getUserProject(this.currentUserId, (project) => {
+      this.userProject = project;
+    });
+  }
+
+  // TODO: join project
+  onSubmitJoin() {
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.joinForm.invalid) {
+      return;
+    }
+
+    this.submitting = true;
+
+    this.backendService.joinProject(
+      this.joinForm.value as ProjectJoin, this.currentUserId
+    ).then((project) => {
+      this.userProject = project;
+      this.submitting = false;
+      this.joinForm.reset();
+    }).catch((error) => {
+      this.alertService.error(error);
+      this.submitting = false;
+    });
+  }
+
+  onSubmitCreate() {
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.createForm.invalid) {
+      return;
+    }
+
+    this.submitting = true;
+
+    this.backendService.createProjectAndAsignToUser(
+      this.createForm.value as ProjectCreation, this.currentUserId
+    ).then((createdProject) => {
+      this.submitting = false;
+      this.userProject = createdProject;
+      this.createForm.reset();
+    }).catch((error) => {
+      this.alertService.error(error);
+      this.submitting = false;
+    });
+  }
 }
