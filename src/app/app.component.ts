d996f20840bb2efa7cb6829269d927c157aa8623
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-02-03 22:36:39 +0100

    feat: add account deletion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AlertComponent } from './_components/alert/alert.component';
 import { AuthService } from './services/auth.service';
-import { sha256 } from 'js-sha256';
+import { BackendService } from './services/backend.service';
+import { AlertService } from './services/alert.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,16 @@ import { sha256 } from 'js-sha256';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent{
+export class AppComponent {
   title = 'mileage-log';
 
   authService = inject(AuthService);
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private backendService: BackendService, private alertService: AlertService) { }
+
+  getUser() {
+    return this.authService.accountValue;
+  }
 
   getUserPhoto() {
     return this.authService.accountValue?.photoURL || 'https://www.gravatar.com/avatar/';
@@ -30,6 +35,24 @@ export class AppComponent{
       this.router.navigate(['/']);
     }).catch((error) => {
       console.error("Error logging out", error);
+    });
+  }
+
+  deleteAccount() {
+    const user = this.getUser();
+    if (!user) {
+      console.debug("No user to delete");
+      return;
+    }
+    this.backendService.deleteAccount(user).then(() => {
+      this.router.navigate(['/']);
+    }).catch((error) => {
+      if (error.code === 'auth/requires-recent-login') {
+        this.alertService.error(`
+          <span>User requires recent login</span>
+          <p>Log out and log in again to delete account.</p>
+        `);
+      }
     });
   }
 }
