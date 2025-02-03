205a9add2c99f6e4be6cc596cd5907a3aeefc47f
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-02-03 04:53:28 +0100

    feat: add user registration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserToLogIn } from '../interfaces/user.interface';
+import { UserToLogIn, UserToRegister } from '../interfaces/user.interface';
 import { AuthService } from '../services/auth.service';
 import { AlertService } from '@app/services/alert.service';
 import { BackendService } from '@app/services/backend.service';
@@ -17,7 +17,15 @@ import { BackendService } from '@app/services/backend.service';
 export class HomeComponent {
   submitting = false;
 
-  form = new FormGroup({
+  loginActive = true;
+
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  })
+
+  formRegister = new FormGroup({
+    fullName: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   })
@@ -29,28 +37,60 @@ export class HomeComponent {
     private backendService: BackendService
   ) { }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
-
-  onSubmit() {
-    if (this.form.invalid) {
+  onSubmitLogin() {
+    if (this.formLogin.invalid) {
       return;
     }
-    this.loginService.logIn(this.form.value as UserToLogIn)
-      .then((userCred) => {
-        this.backendService.createUser(userCred);
+    this.loginService.logIn(this.formLogin.value as UserToLogIn)
+      .then(() => {
         this.router.navigate(['/projects']);
       })
       .catch(() => {
         this.alertService.error("Email or password is incorrect");
+      })
+      .finally(() => {
+        this.submitting = false;
+      });
+  }
+
+  onSubmitRegister() {
+    if (this.formRegister.invalid) {
+      return;
+    }
+    this.submitting = true;
+    this.loginService.register(this.formRegister.value as UserToRegister)
+      .then((userCred) => {
+        this.backendService.createUser(userCred, this.formRegister.value as UserToRegister);
+        this.router.navigate(['/projects']);
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/email-already-in-use':
+            this.alertService.error("Email already in use");
+            break;
+          case 'auth/invalid-email':
+            this.alertService.error("Invalid email address");
+            break;
+          case 'auth/operation-not-allowed':
+            this.alertService.error("Operation not allowed");
+            break;
+          case 'auth/weak-password':
+            this.alertService.error("Password is too weak");
+            break;
+          default:
+            this.alertService.error("An unknown error occurred");
+        }
+      })
+      .finally(() => {
+        this.submitting = false;
       });
   }
 
   onClickGoogle() {
     this.loginService.logInGoogle()
-    .then((userCred) => {
-      this.backendService.createUser(userCred);
-      this.router.navigate(['/projects']);
-    }).catch((error) => console.error(error));
+      .then((userCred) => {
+        this.backendService.createUserWithGoogle(userCred);
+        this.router.navigate(['/projects']);
+      }).catch((error) => console.error(error));
   }
 }
