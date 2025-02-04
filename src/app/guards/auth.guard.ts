e81fe4ef5fa6e5c9394e3654238754c968f2c768
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-02-04 21:36:45 +0100

    refactor: fix auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,16 +1,22 @@
 import { inject } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { map, take } from 'rxjs/operators';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const router = inject(Router);
-  const authService = inject(AuthService);
+  const afa = inject(AngularFireAuth)
 
-  if (authService.isAuthenticated()) {
-    return true;
-  }
-  
-  router.navigate(['/']);
-  return false;
+  return afa.authState.pipe(
+    take(1),
+    map(user => {
+      if (user) {
+        return true;
+      }
+      router.navigate(['/']);
+      return false;
+    })
+  );
 };
