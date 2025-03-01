3d193ca4802bfd323aa1d24c791e066de90bc9b5
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-03-01 18:07:49 +0100

    feat: now data can be exported to a CSV file
    - also added a debug mode for adding trips in name of other users

--- src/app/projects/view-project/view-project.component.ts
+++ src/app/projects/view-project/view-project.component.ts
@@ -9,7 +9,7 @@ import { TripCreation, TripDTO } from '@app/interfaces/trip.interface';
 import { AlertService } from '@app/services/alert.service';
 import { AuthService } from '@app/services/auth.service';
 import { BackendService } from '@app/services/backend.service';
-import { format } from '@formkit/tempo';
+import { monthDays } from '@formkit/tempo';
 import { getDatabase, ref } from "firebase/database";
 
 @Component({
@@ -23,7 +23,11 @@ export class ViewProjectComponent implements OnInit {
   @Input() project!: ProjectDTO;
   @Output() hasProject = new EventEmitter<null>();
 
+  debugMode = false;
+
   form = new FormGroup({
+    // [ DEBUG ]: Para testear la creación de un viaje con un usuario distinto
+    tripUserId: new FormControl({ value: '', disabled: !this.debugMode }, Validators.required),
     tripDate: new FormControl('', Validators.required),
     tripStartKm: new FormControl('', Validators.required),
     tripEndKm: new FormControl('', Validators.required),
@@ -46,6 +50,7 @@ export class ViewProjectComponent implements OnInit {
 
   currentUserRole: string = 'user';
 
+  firstTrip: TripDTO | null = null;
   lastTrip: TripDTO | null = null;
 
   database = getDatabase();
@@ -91,12 +96,16 @@ export class ViewProjectComponent implements OnInit {
       tripEndKm: Number(this.form.value.tripEndKm),
     };
 
+    // [ DEBUG ]: Para testear la creación de un viaje con un usuario distinto
+    if (this.debugMode) {
+      this.currentUserId = this.form.value.tripUserId!;
+    }
     this.backendService.createNewTrip(
       newTrip, this.currentUserId, this.project
     ).then(() => {
       console.debug("Trip created");
       this.form.reset({
-        tripDate: format(new Date(), "YYYY-MM-DD"),
+        tripDate: '',
         tripStartKm: '',
         tripEndKm: ''
       });
@@ -155,7 +164,13 @@ export class ViewProjectComponent implements OnInit {
       this.project.projectTotalKms = trips.reduce((acc, trip) => acc + trip.tripTotalKms, 0);
 
       // Actualizamos lastTrip con el último viaje realizado
-      this.lastTrip = trips.length > 0 ? trips[trips.length - 1] : null;
+      // this.lastTrip = trips.length > 0 ? trips[trips.length - 1] : null;
+      this.lastTrip = trips.reduce((latestTrip, currentTrip) => {
+        return new Date(currentTrip.tripDate) >= new Date(latestTrip.tripDate) ? currentTrip : latestTrip;
+      }, trips[0]);
+      this.firstTrip = trips.reduce((earliestTrip, currentTrip) => {
+        return new Date(currentTrip.tripDate) <= new Date(earliestTrip.tripDate) ? currentTrip : earliestTrip;
+      }, trips[0]);
 
       // Iterador para obtener colores
       const iterator = this.generateColors();
@@ -172,10 +187,39 @@ export class ViewProjectComponent implements OnInit {
         };
       });
       console.debug("Datasets obtenidos", this.myData.datasets);
+      // Las etiquetas se desordenan, debemos regenerarlas
+      this.myData.labels = this.generateLabels();
       this.myData = { ...this.myData };
     });
   }
 
+  generateLabels() {
+    const currentMonth = new Date().getMonth();
+    const projectStartMonth = new Date(this.project.projectStartDate).getMonth();
+
+    if (currentMonth === projectStartMonth) {
+      const daysInMonth = monthDays(new Date());
+      const today = new Date().getDate();
+      const labels = [];
+
+      for (let i = 1; i <= daysInMonth; i++) {
+        labels.push(i < 10 ? `0${i}` : `${i}`);
+      }
+
+      return labels.slice(0, today);
+    } else {
+      const startDate = new Date(this.project.projectStartDate);
+      const endDate = new Date(this.project.projectEndDate);
+      const labels = [];
+
+      for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+        const day = d.getDate();
+        labels.push(day < 10 ? `0${day}` : `${day}`);
+      }
+
+      return labels;
+    }
+  }
 
   *generateColors(): IterableIterator<{ rgb: string, rgba: string }> {
     const colors = [
@@ -183,8 +227,7 @@ export class ViewProjectComponent implements OnInit {
       { rgb: 'rgb(54, 162, 235)', rgba: 'rgba(54, 162, 235, 0.5)' },
       { rgb: 'rgb(255, 206, 86)', rgba: 'rgba(255, 206, 86, 0.5)' },
       { rgb: 'rgb(75, 192, 192)', rgba: 'rgba(75, 192, 192, 0.5)' },
-      { rgb: 'rgb(153, 102, 255)', rgba: 'rgba(153, 102, 255, 0.5)' },
-      { rgb: 'rgb(255, 159, 64)', rgba: 'rgba(255, 159, 64, 0.5)' }
+      { rgb: 'rgb(153, 102, 255)', rgba: 'rgba(153, 102, 255, 0.5)' }
     ];
 
     let index = 0;
@@ -226,7 +269,7 @@ export class ViewProjectComponent implements OnInit {
   }
 
   // Calculadora de uso por usuario
-  
+
   calculateUsage(user: any) {
     const totalKms = this.project.projectTotalKms;
     if (totalKms === 0) {
@@ -256,6 +299,74 @@ export class ViewProjectComponent implements OnInit {
 
   calculateUserCost(user: any): number {
     return this.costPerUser[user.userId] || 0;
+  }
+
+  exportData() {
+    console.debug("Exporting data...", this.projectUsers);
+
+    const csv = this.convertToCSV();
+    const a = document.createElement('a');
+    a.setAttribute('style', 'display:none;');
+    document.body.appendChild(a);
+    a.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    a.target = '_blank';
+    const projectName = this.project.projectName.toLowerCase().replace(' ', '_');
+    a.download = 'proyecto_' + projectName + '.csv';
+    a.click();
+  }
+
+  convertToCSV() {
+    const rows = 8;
+    const cols = 7;
+    const matrix = new Array(rows).fill(0).map(() => new Array(cols).fill(''));
+
+    matrix[0][0] = 'Proyecto:';
+    matrix[0][1] = this.project.projectName;
+    matrix[1][0] = 'Fecha Inicio:';
+    matrix[1][1] = this.project.projectStartDate;
+    matrix[2][0] = 'Fecha Fin:';
+    matrix[2][1] = this.project.projectEndDate;
+
+    matrix[3][0] = 'Km iniciales:';
+    matrix[3][1] = this.firstTrip?.tripStartKm;
+    matrix[4][0] = 'Km finales:';
+    matrix[4][1] = this.lastTrip?.tripEndKm;
+    matrix[5][0] = 'Km Totales:';
+    matrix[5][1] = this.project.projectTotalKms;
+
+    const costPerKm = this.totalGasolina / this.project.projectTotalKms;
+
+    matrix[6][0] = 'Total Gasolina (EUR)';
+    matrix[6][1] = this.totalGasolina;
+    matrix[7][0] = 'Coste por km (EUR/km)';
+    matrix[7][1] = costPerKm;
+
+    matrix[0][3] = 'Usuario';
+    matrix[0][4] = 'Km Totales';
+    matrix[0][5] = 'Porc. de Uso';
+    matrix[0][6] = 'Gasto (EUR)';
+
+    for (let i = 0; i < this.projectUsers.length; i++) {
+      matrix[i + 1][3] = this.projectUsers[i].userDisplayName;
+      matrix[i + 1][4] = this.projectUsers[i].userProject.projectKms;
+      matrix[i + 1][5] = this.calculateUsage(this.projectUsers[i]) + '%';
+      matrix[i + 1][6] = Math.round(this.projectUsers[i].userProject.projectKms * costPerKm * 100) / 100;
+    }
+
+    // console.debug("Matrix", matrix);
+
+    let toExport = ''
+
+    for (let i = 0; i < rows; i++) {
+      let line = '';
+      for (let j = 0; j < cols; j++) {
+        if (line != '') line += ';';
+        line += matrix[i][j];
+      }
+      toExport += line + '\r\n';
+    }
+    // console.debug("CSV to export\n", toExport);
+    return toExport;
   }
 
   // Chart options
