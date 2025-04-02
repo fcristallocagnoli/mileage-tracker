0a1a0f55d52f975f301526f76dca255b9cf13608
Fabrizio Cristallo <fcristallocagnoli@gmail.com>
2025-04-02 02:36:32 +0200

    refactor: improve range validator
    - now is generic. reusable for any date field

--- src/app/_helpers/must-be-in-range.validator.ts
+++ src/app/_helpers/must-be-in-range.validator.ts
@@ -1,30 +1,34 @@
 import { AbstractControl } from '@angular/forms';
+import { ProjectDTO } from '@app/interfaces/project.interface';
 
-// custom validator to check that tripEndKm is greater than tripStartKm
-export function MustBeInRange(startDate: string, endDate: string) {
+// custom validator to check that a date field is between projectStartDate and projectEndDate
+export function MustBeInRange(dateFieldRaw: string, project: ProjectDTO) {
   return (group: AbstractControl) => {
-    const tripDate = group.get('tripDate');
+    const startDate = project.projectStartDate;
+    const endDate = project.projectEndDate;
+    
+    const dateField = group.get(dateFieldRaw);
 
-    // trip date debe estar entre project.projectStartDate y project.projectEndDate
+    // dateField debe estar entre project.projectStartDate y project.projectEndDate
 
-    if (!tripDate) {
+    if (!dateField) {
       return null;
     }
 
-    // return if another validator has already found an error on the tripDate
-    if (tripDate.errors && !tripDate.errors['mustBeInRange']) {
+    // return if another validator has already found an error on the dateField
+    if (dateField.errors && !dateField.errors['mustBeInRange']) {
       return null;
     }
 
-    const tripDateValue = new Date(group.get('tripDate')?.value);
+    const dateFieldValue = new Date(dateField?.value);
     const projectStartDate = new Date(startDate);
     const projectEndDate = new Date(endDate);
 
-    // // set error on tripDate if validation fails
-    if (tripDateValue < projectStartDate || tripDateValue > projectEndDate) {
-      tripDate.setErrors({ mustBeInRange: true });
+    // set error on dateField if validation fails
+    if (dateFieldValue < projectStartDate || dateFieldValue > projectEndDate) {
+      dateField.setErrors({ mustBeInRange: true });
     } else {
-      tripDate.setErrors(null);
+      dateField.setErrors(null);
     }
 
     return null;
